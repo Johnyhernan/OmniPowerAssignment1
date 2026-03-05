84a31eebd23073f596845dae63e78a49311042ea
J.m Hernandez <johncopro51@gmail.com>
2026-03-05 00:18:42 -0600

    Aadded Sensor class for sensor objects and Sensor_handler to handle data for sensor

--- OmniPowerAssignment/Form1.Designer.cs
+++ OmniPowerAssignment/Form1.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             tableLayoutPanel1 = new TableLayoutPanel();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            textBox1 = new TextBox();
-            label1 = new Label();
-            label2 = new Label();
             label3 = new Label();
+            label2 = new Label();
+            HumidityReading = new TextBox();
+            PressureReading = new TextBox();
+            TempReading = new TextBox();
+            label1 = new Label();
             tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
@@ -45,9 +45,9 @@
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             tableLayoutPanel1.Controls.Add(label3, 0, 2);
             tableLayoutPanel1.Controls.Add(label2, 0, 1);
-            tableLayoutPanel1.Controls.Add(textBox2, 1, 1);
-            tableLayoutPanel1.Controls.Add(textBox3, 1, 2);
-            tableLayoutPanel1.Controls.Add(textBox1, 1, 0);
+            tableLayoutPanel1.Controls.Add(HumidityReading, 1, 1);
+            tableLayoutPanel1.Controls.Add(PressureReading, 1, 2);
+            tableLayoutPanel1.Controls.Add(TempReading, 1, 0);
             tableLayoutPanel1.Controls.Add(label1, 0, 0);
             tableLayoutPanel1.Location = new Point(657, 25);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -57,54 +57,6 @@
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 58F));
             tableLayoutPanel1.Size = new Size(200, 168);
             tableLayoutPanel1.TabIndex = 0;
-            // 
-            // textBox2
-            // 
-            textBox2.Anchor = AnchorStyles.None;
-            textBox2.Location = new Point(103, 72);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(94, 23);
-            textBox2.TabIndex = 1;
-            textBox2.TextChanged += textBox2_TextChanged;
-            // 
-            // textBox3
-            // 
-            textBox3.Anchor = AnchorStyles.None;
-            textBox3.BackColor = SystemColors.Window;
-            textBox3.Location = new Point(103, 127);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(94, 23);
-            textBox3.TabIndex = 2;
-            // 
-            // textBox1
-            // 
-            textBox1.Anchor = AnchorStyles.None;
-            textBox1.Location = new Point(103, 17);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(94, 23);
-            textBox1.TabIndex = 3;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Dock = DockStyle.Fill;
-            label1.Location = new Point(3, 0);
-            label1.Name = "label1";
-            label1.Size = new Size(94, 58);
-            label1.TabIndex = 4;
-            label1.Text = "Temperature(C)";
-            label1.TextAlign = ContentAlignment.MiddleCenter;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Dock = DockStyle.Fill;
-            label2.Location = new Point(3, 58);
-            label2.Name = "label2";
-            label2.Size = new Size(94, 52);
-            label2.TabIndex = 5;
-            label2.Text = "Humidity(%)";
-            label2.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // label3
             // 
@@ -117,6 +69,58 @@
             label3.Text = "Pressure(\\p)";
             label3.TextAlign = ContentAlignment.MiddleCenter;
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Dock = DockStyle.Fill;
+            label2.Location = new Point(3, 58);
+            label2.Name = "label2";
+            label2.Size = new Size(94, 52);
+            label2.TabIndex = 5;
+            label2.Text = "Humidity(%)";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // HumidityReading
+            // 
+            HumidityReading.Anchor = AnchorStyles.None;
+            HumidityReading.BackColor = SystemColors.ControlLightLight;
+            HumidityReading.Location = new Point(103, 72);
+            HumidityReading.Name = "HumidityReading";
+            HumidityReading.ReadOnly = true;
+            HumidityReading.Size = new Size(94, 23);
+            HumidityReading.TabIndex = 1;
+            // 
+            // PressureReading
+            // 
+            PressureReading.Anchor = AnchorStyles.None;
+            PressureReading.BackColor = SystemColors.Window;
+            PressureReading.Location = new Point(103, 127);
+            PressureReading.Name = "PressureReading";
+            PressureReading.ReadOnly = true;
+            PressureReading.Size = new Size(94, 23);
+            PressureReading.TabIndex = 2;
+            // 
+            // TempReading
+            // 
+            TempReading.Anchor = AnchorStyles.None;
+            TempReading.BackColor = SystemColors.ControlLightLight;
+            TempReading.Location = new Point(103, 17);
+            TempReading.Name = "TempReading";
+            TempReading.ReadOnly = true;
+            TempReading.Size = new Size(94, 23);
+            TempReading.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Dock = DockStyle.Fill;
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(94, 58);
+            label1.TabIndex = 4;
+            label1.Text = "Temperature(C)";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -125,7 +129,6 @@
             Controls.Add(tableLayoutPanel1);
             Name = "Form1";
             Text = "Form1";
-            Load += Form1_Load;
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel1.PerformLayout();
             ResumeLayout(false);
@@ -134,11 +137,11 @@
         #endregion
 
         private TableLayoutPanel tableLayoutPanel1;
-        private TextBox textBox2;
-        private TextBox textBox3;
+        private TextBox HumidityReading;
+        private TextBox PressureReading;
         private Label label3;
         private Label label2;
-        private TextBox textBox1;
+        private TextBox TempReading;
         private Label label1;
     }
 }
--- OmniPowerAssignment/Form1.cs
+++ OmniPowerAssignment/Form1.cs
@@ -2,18 +2,25 @@ namespace OmniPowerAssignment
 {
     public partial class Form1 : Form
     {
+        private Sensor_Handler SensorHandler1 = new Sensor_Handler();
+        private System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+
         public Form1()
         {
             InitializeComponent();
+            timer.Interval = 1500;
+
+            timer.Tick += UpdateUI;
+            timer.Start();
+
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void UpdateUI(object sender, EventArgs e)
         {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
+            SensorHandler1.Update_Values();
+            TempReading.Text = SensorHandler1.Temp_History.Last().ToString();
+            HumidityReading.Text = SensorHandler1.Humidity_History.Last().ToString();
+            PressureReading.Text = SensorHandler1.Pressure_History.Last().ToString();
 
         }
     }
