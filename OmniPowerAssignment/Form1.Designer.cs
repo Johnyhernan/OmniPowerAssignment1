fbe5afb176257fb6fe9a79faa945694c29d67bfb
J.m Hernandez <johncopro51@gmail.com>
2026-03-05 00:59:00 -0600

    Added graph functionalities.

--- OmniPowerAssignment/Form1.Designer.cs
+++ OmniPowerAssignment/Form1.Designer.cs
@@ -35,36 +35,47 @@
             PressureReading = new TextBox();
             TempReading = new TextBox();
             label1 = new Label();
+            Graph = new MenuStrip();
+            graphToolStripMenuItem = new ToolStripMenuItem();
+            temperatureToolStripMenuItem = new ToolStripMenuItem();
+            pressureToolStripMenuItem = new ToolStripMenuItem();
+            humidityToolStripMenuItem = new ToolStripMenuItem();
+            formsPlot1 = new ScottPlot.WinForms.FormsPlot();
+            trackBar1 = new TrackBar();
             tableLayoutPanel1.SuspendLayout();
+            Graph.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
             SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             tableLayoutPanel1.ColumnCount = 2;
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 48F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 52F));
             tableLayoutPanel1.Controls.Add(label3, 0, 2);
             tableLayoutPanel1.Controls.Add(label2, 0, 1);
             tableLayoutPanel1.Controls.Add(HumidityReading, 1, 1);
             tableLayoutPanel1.Controls.Add(PressureReading, 1, 2);
             tableLayoutPanel1.Controls.Add(TempReading, 1, 0);
             tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Controls.Add(Graph, 1, 3);
             tableLayoutPanel1.Location = new Point(657, 25);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowCount = 4;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 52F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 58F));
-            tableLayoutPanel1.Size = new Size(200, 168);
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 60F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 44F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 44F));
+            tableLayoutPanel1.Size = new Size(200, 251);
             tableLayoutPanel1.TabIndex = 0;
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Dock = DockStyle.Fill;
-            label3.Location = new Point(3, 110);
+            label3.Location = new Point(3, 163);
             label3.Name = "label3";
-            label3.Size = new Size(94, 58);
+            label3.Size = new Size(90, 44);
             label3.TabIndex = 6;
             label3.Text = "Pressure(\\p)";
             label3.TextAlign = ContentAlignment.MiddleCenter;
@@ -73,9 +84,9 @@
             // 
             label2.AutoSize = true;
             label2.Dock = DockStyle.Fill;
-            label2.Location = new Point(3, 58);
+            label2.Location = new Point(3, 103);
             label2.Name = "label2";
-            label2.Size = new Size(94, 52);
+            label2.Size = new Size(90, 60);
             label2.TabIndex = 5;
             label2.Text = "Humidity(%)";
             label2.TextAlign = ContentAlignment.MiddleCenter;
@@ -84,7 +95,7 @@
             // 
             HumidityReading.Anchor = AnchorStyles.None;
             HumidityReading.BackColor = SystemColors.ControlLightLight;
-            HumidityReading.Location = new Point(103, 72);
+            HumidityReading.Location = new Point(101, 121);
             HumidityReading.Name = "HumidityReading";
             HumidityReading.ReadOnly = true;
             HumidityReading.Size = new Size(94, 23);
@@ -94,7 +105,7 @@
             // 
             PressureReading.Anchor = AnchorStyles.None;
             PressureReading.BackColor = SystemColors.Window;
-            PressureReading.Location = new Point(103, 127);
+            PressureReading.Location = new Point(101, 173);
             PressureReading.Name = "PressureReading";
             PressureReading.ReadOnly = true;
             PressureReading.Size = new Size(94, 23);
@@ -104,7 +115,7 @@
             // 
             TempReading.Anchor = AnchorStyles.None;
             TempReading.BackColor = SystemColors.ControlLightLight;
-            TempReading.Location = new Point(103, 17);
+            TempReading.Location = new Point(101, 40);
             TempReading.Name = "TempReading";
             TempReading.ReadOnly = true;
             TempReading.Size = new Size(94, 23);
@@ -116,22 +127,83 @@
             label1.Dock = DockStyle.Fill;
             label1.Location = new Point(3, 0);
             label1.Name = "label1";
-            label1.Size = new Size(94, 58);
+            label1.Size = new Size(90, 103);
             label1.TabIndex = 4;
             label1.Text = "Temperature(C)";
             label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Graph
+            // 
+            Graph.BackColor = SystemColors.ControlLightLight;
+            Graph.Dock = DockStyle.None;
+            Graph.Items.AddRange(new ToolStripItem[] { graphToolStripMenuItem });
+            Graph.Location = new Point(96, 207);
+            Graph.Name = "Graph";
+            Graph.RenderMode = ToolStripRenderMode.Professional;
+            Graph.Size = new Size(59, 24);
+            Graph.TabIndex = 7;
+            Graph.Text = "menuStrip1";
+            // 
+            // graphToolStripMenuItem
+            // 
+            graphToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { temperatureToolStripMenuItem, pressureToolStripMenuItem, humidityToolStripMenuItem });
+            graphToolStripMenuItem.Name = "graphToolStripMenuItem";
+            graphToolStripMenuItem.Size = new Size(51, 20);
+            graphToolStripMenuItem.Text = "Graph";
+            // 
+            // temperatureToolStripMenuItem
+            // 
+            temperatureToolStripMenuItem.Name = "temperatureToolStripMenuItem";
+            temperatureToolStripMenuItem.Size = new Size(180, 22);
+            temperatureToolStripMenuItem.Text = "Temperature";
+            temperatureToolStripMenuItem.Click += temperatureToolStripMenuItem_Click;
+            // 
+            // pressureToolStripMenuItem
+            // 
+            pressureToolStripMenuItem.Name = "pressureToolStripMenuItem";
+            pressureToolStripMenuItem.Size = new Size(180, 22);
+            pressureToolStripMenuItem.Text = "Pressure";
+            pressureToolStripMenuItem.Click += pressureToolStripMenuItem_Click;
+            // 
+            // humidityToolStripMenuItem
+            // 
+            humidityToolStripMenuItem.Name = "humidityToolStripMenuItem";
+            humidityToolStripMenuItem.Size = new Size(180, 22);
+            humidityToolStripMenuItem.Text = "Humidity";
+            humidityToolStripMenuItem.Click += humidityToolStripMenuItem_Click;
+            // 
+            // formsPlot1
+            // 
+            formsPlot1.DisplayScale = 1F;
+            formsPlot1.Location = new Point(21, 32);
+            formsPlot1.Name = "formsPlot1";
+            formsPlot1.Size = new Size(611, 296);
+            formsPlot1.TabIndex = 7;
+            // 
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(669, 283);
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(183, 45);
+            trackBar1.TabIndex = 8;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(869, 450);
+            ClientSize = new Size(869, 374);
+            Controls.Add(trackBar1);
             Controls.Add(tableLayoutPanel1);
+            Controls.Add(formsPlot1);
             Name = "Form1";
             Text = "Form1";
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel1.PerformLayout();
+            Graph.ResumeLayout(false);
+            Graph.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -143,5 +215,12 @@
         private Label label2;
         private TextBox TempReading;
         private Label label1;
+        private ScottPlot.WinForms.FormsPlot formsPlot1;
+        private MenuStrip Graph;
+        private ToolStripMenuItem graphToolStripMenuItem;
+        private ToolStripMenuItem temperatureToolStripMenuItem;
+        private ToolStripMenuItem pressureToolStripMenuItem;
+        private ToolStripMenuItem humidityToolStripMenuItem;
+        private TrackBar trackBar1;
     }
 }
--- OmniPowerAssignment/Form1.cs
+++ OmniPowerAssignment/Form1.cs
@@ -8,20 +8,83 @@ namespace OmniPowerAssignment
         public Form1()
         {
             InitializeComponent();
-            timer.Interval = 1500;
 
-            timer.Tick += UpdateUI;
+            formsPlot1.Plot.Title("Temperature Over Time");
+            formsPlot1.Plot.YLabel("°C");
+            formsPlot1.Plot.XLabel("Time");
+            timer.Interval = 1500;//every 1.5 seconds refresh and get new values
+
+            timer.Tick += UpdateUI;//call updateUI each tick cycle 
+
             timer.Start();
 
         }
 
         private void UpdateUI(object sender, EventArgs e)
         {
+            formsPlot1.Plot.Clear();
             SensorHandler1.Update_Values();
             TempReading.Text = SensorHandler1.Temp_History.Last().ToString();
             HumidityReading.Text = SensorHandler1.Humidity_History.Last().ToString();
             PressureReading.Text = SensorHandler1.Pressure_History.Last().ToString();
 
+
+            double[] data = SensorHandler1.Temp_History.ToArray();
+            double[] data2 = SensorHandler1.TimeStamp.ToArray();
+            formsPlot1.Plot.Add.Scatter(data2, data);
+            formsPlot1.Plot.Axes.DateTimeTicksBottom();
+            formsPlot1.Plot.Axes.AutoScale();
+
+            formsPlot1.Refresh();
+
+
+        }
+
+        private void temperatureToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            formsPlot1.Plot.Title("Temperature Over Time");
+            formsPlot1.Plot.YLabel("°C");
+
+            formsPlot1.Plot.Clear();
+            double[] data = SensorHandler1.Temp_History.ToArray();
+            double[] data2 = SensorHandler1.TimeStamp.ToArray();
+            formsPlot1.Plot.Add.Scatter(data2, data);
+            formsPlot1.Plot.Axes.DateTimeTicksBottom();
+            formsPlot1.Plot.Axes.AutoScale();
+
+            formsPlot1.Refresh();
+
+        }
+
+        private void pressureToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            formsPlot1.Plot.Title("Pressure Over Time");
+            formsPlot1.Plot.YLabel("p");
+
+            formsPlot1.Plot.Clear();
+            double[] data = SensorHandler1.Pressure_History.ToArray();
+            double[] data2 = SensorHandler1.TimeStamp.ToArray();
+            formsPlot1.Plot.Add.Scatter(data2, data);
+            formsPlot1.Plot.Axes.DateTimeTicksBottom();
+            formsPlot1.Plot.Axes.AutoScale();
+
+            formsPlot1.Refresh();
+
+        }
+
+        private void humidityToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            formsPlot1.Plot.Title("Humidity Over Time");
+            formsPlot1.Plot.YLabel("%");
+
+            formsPlot1.Plot.Clear();
+            double[] data = SensorHandler1.Humidity_History.ToArray();
+            double[] data2 = SensorHandler1.TimeStamp.ToArray();
+            formsPlot1.Plot.Add.Scatter(data2, data);
+            formsPlot1.Plot.Axes.DateTimeTicksBottom();
+            formsPlot1.Plot.Axes.AutoScale();
+
+            formsPlot1.Refresh();
         }
     }
 }
